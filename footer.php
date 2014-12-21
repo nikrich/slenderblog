8591aaecdf808ff194acb7ee28461bdbb9bc3f63
unknown <jannik811@gmail.com>
2014-12-21 10:00:09 +0200

    Scaffolding complete

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * slenderblog template for displaying the footer
+ *
+ * @package WordPress
+ * @subpackage slenderblog
+ * @since slenderblog 1.0
+ */
+?>
+
+				<ul class="footer-widgets"><?php
+					if ( function_exists( 'dynamic_sidebar' ) ) :
+						dynamic_sidebar( 'footer-sidebar' );
+					endif; ?>
+				</ul>
+
+			</div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ single.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * slenderblog template for displaying Single-Posts
+ *
+ * @package WordPress
+ * @subpackage slenderblog
+ * @since slenderblog 1.0
+ */
+
+get_header(); ?>
+
+	<section class="page-content primary" role="main">
+
+		<?php
+			if ( have_posts() ) : the_post();
+
+				get_template_part( 'loop', get_post_format() ); ?>
+
+				<aside class="post-aside">
+
+					<div class="post-links">
+						<?php previous_post_link( '%link', __( '&laquo; Previous post', 'slenderblog' ) ) ?>
+						<?php next_post_link( '%link', __( 'Next post &raquo;', 'slenderblog' ) ); ?>
+					</div>
+
+					<?php
+						if ( comments_open() || get_comments_number() > 0 ) :
+							comments_template( '', true );
+						endif;
+					?>
+
+				</aside><?php
+
+			else :
+
+				get_template_part( 'loop', 'empty' );
+
+			endif;
+		?>
+
+	</section>
+
+<?php get_footer(); ?>
